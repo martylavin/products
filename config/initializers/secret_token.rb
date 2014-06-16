363cb7f6fc297e294154e8708d5cbe64669eb7c4
mlavin <martylavin1@yahoo.com>
2014-06-16 11:04:15 -0500

    initial commit for test pgm products

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Products::Application.config.secret_key_base = '9003768a4e343ab6d5aa5fb693a3bb8f10da5077e357bd2ff65a17285b22ed6958826c6dcde034222c89ce17fefce1937d0d8e9253e69eff1d5beebd50f578a6'
